1c08b3a970aa51704c5d5017232793af7c05ee0f
brian <brian6peng@126.com>
2014-09-09 20:54:32 +0800

    StandardOutput

---
+++ PdfExtraction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("pdfinfo.exe", "01.pdf");
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardOutput = true;
+            Process process = Process.Start(processStartInfo);
+            string info = process.StandardOutput.ReadToEnd();
+            Console.WriteLine(info);
+            Console.Read();
+        }
+    }
+}
